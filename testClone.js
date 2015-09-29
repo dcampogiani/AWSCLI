f63cbdf34016372ee2ede46e10f7b5fbf6affb68
Alfredo Moretta <alfredo.moretta@getconnected.it>
2015-09-29 16:17:20 +0200

    da me funziona
    3 righe di codice

--- testClone.js
+++ testClone.js
@@ -2,16 +2,12 @@ var config = require('./config.json');
 var token = config.gitHubAccessToken;
 var user = config.testProjectDetails.username;
 var repo = config.testProjectDetails.repo;
-var clone = require('git-clone');
+var Git = require("nodegit");
 
 var repoUrl = 'https://' + token + '@github.com/' + user + '/' + repo + '.git';
 console.log('Trying to clone ' + repoUrl);
 
 
-clone('git@github.com:jaz303/tpl-simple-site.git', './test-checkout', {
-    checkout: 'a76362b0705d4126fa4462916cabb2506ecfe8e2'
-  },
-  function(err) {
-    console.log("complete!");
-    console.log(err);
-  });
+Git.Clone(repoUrl, "CalabashTest").then(function(repository) {
+  // Work with the repository object here.
+});
