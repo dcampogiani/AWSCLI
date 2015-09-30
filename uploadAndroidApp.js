709fcd01c2b2248d79205afedecf9133f8919b61
Daniele Campogiani <daniele.campogiani@gmail.com>
2015-09-30 17:16:53 +0200

    WIP: upload scripts

--- uploadAndroidApp.js
+++ uploadAndroidApp.js
@@ -1,17 +1,31 @@
 var AWS = require('aws-sdk');
-var config = require('config.json');
+var config = require('./config.json');
+
+var apkPath = process.argv[2];
+
+if (apkPath == undefined) {
+  console.log("Usage: node uploadAndroidApp.js path_to_app.apk")
+  return;
+}
 
 var deviceFarm = new AWS.DeviceFarm();
 
+AWS.config.update({
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey
+});
+
 var createUploadParams = {
-  name: 'STRING_VALUE',
+  name: apkPath,
   projectArn: config.projectArn,
   type: 'ANDROID_APP'
 }
 
 var createUploadCallback = function(err, data) {
-
+  if (err)
+    console.log(err, err.stack);
+  else
+    console.log(data);
 }
 
-
-deviceFarm.createUpload(createUploadParams, createUploadCallback);
+var request = deviceFarm.createUpload(createUploadParams, createUploadCallback);
--- uploadTests.js
+++ uploadTests.js
@@ -1,17 +1,31 @@
 var AWS = require('aws-sdk');
-var wget = require('wget');
 var config = require('config.json');
+
+var testsPath = process.argv[2];
+
+if (testsPath == undefined) {
+  console.log("Usage: node uploadTests.js path_to_tests.zip")
+  return;
+}
 
 var deviceFarm = new AWS.DeviceFarm();
 
+AWS.config.update({
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey
+});
+
 var createUploadParams = {
-  name: 'STRING_VALUE',
+  name: testsPath,
   projectArn: config.projectArn,
   type: 'CALABASH_TEST_PACKAGE'
 }
 
 var createUploadCallback = function(err, data) {
-
+  if (err)
+    console.log(err, err.stack);
+  else
+    console.log(data);
 }
 
 
